dfd88625c582e1370d696f6a15d6d25fe1d9ccee
Shashi Kant <shashi1729kant@gmail.com>
2023-03-11 01:17:10 +0530

    Updating Contact Page

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,22 +1,22 @@
 import React from "react";
-import { FaBirthdayCake } from "react-icons/fa";
 import { BsFlag, BsFillTelephoneFill } from "react-icons/bs";
 import { HiLocationMarker } from "react-icons/hi";
 import { GrMail } from "react-icons/gr";
-import { AiOutlineSetting, AiOutlineHeart } from "react-icons/ai";
-import { MdOutlineCases } from "react-icons/md";
-import { BiLike } from "react-icons/bi";
+import { RiFacebookFill } from "react-icons/ri";
+import { FiTwitter, FiMail } from "react-icons/fi";
+import { SlSocialLinkedin } from "react-icons/sl";
+import { BsInstagram } from "react-icons/bs";
 
 function MyApp() {
 	return (
-		<div className="flex flex-col w-full h-full">
+		<div className="flex flex-col w-full h-full overflow-y-auto ">
 			<div className="flex text-4xl md:text-5xl lg:text-6xl h-40 w-full text-center align-self-center lg:mb-12">
 				<div className="m-auto mt-20">
 					Get <span className="text-pink-600 font-abfat">In Touch</span>
 				</div>
 			</div>
 			<div className="flex flex-col">
-				<div className="flex gap-4 flex-col lg:flex-row justify-around lg:p-0 p-8 ">
+				<div className="flex gap-4 flex-col lg:flex-row justify-around lg:p-6 p-8 ">
 					<div
 						className="bg-slate-100  px-12 text-center place-items-center rounded-md "
 						style={{ textAlign: "-webkit-center" }}
@@ -66,16 +66,36 @@ function MyApp() {
 						</div>
 					</div>
 				</div>
-				<div className="flex lg:flex-row flex-col p-4">
+				<div className="flex lg:flex-row flex-col lg:p-6 p-8 ">
 					<div className="flex lg:flex-col flex-row w-1/3">
 						<div className="pt-8">
-							<p className="text-xl py-4 font-abfat">Send Me An Email</p>
-							Feel free to get in touch with me. I am always open to discussing
-							new projects or creative ideas.
+							<p className="text-4xl py-4 font-serif">Send Me An Email</p>
+							<p className="text-md tracking-wider">
+								Feel free to get in touch with me. I am always open to
+								discussing new projects or creative ideas.
+							</p>
 						</div>
-						<div className=""></div>
+						<div className="pt-4">
+							<p className="text-4xl py-4 font-thin">
+								Follow <span className="text-pink-600">Me</span>
+							</p>
+							<div className="flex flex-row gap-10">
+								<button className="p-3 bg-slate-200 rounded-3xl text-xl hover:bg-pink-600 hover:text-white ">
+									<RiFacebookFill />
+								</button>
+								<button className="p-3 bg-slate-200 rounded-3xl text-xl hover:bg-pink-600 hover:text-white">
+									<FiTwitter />
+								</button>
+								<button className="p-3 bg-slate-200 rounded-3xl text-xl hover:bg-pink-600 hover:text-white">
+									<SlSocialLinkedin />
+								</button>
+								<button className="p-3 bg-slate-200 rounded-3xl text-xl hover:bg-pink-600 hover:text-white">
+									<BsInstagram />
+								</button>
+							</div>
+						</div>
 					</div>
-					<div className="w-2/3">
+					<div className="w-2/3 p-10">
 						<input
 							className="w-32 border-1 border-pink-600"
 							type="text"
